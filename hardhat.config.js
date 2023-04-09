e901e39aab2c5ed685c78caf92931adcb5a75099
Totenfluch <github@cziegler.de>
2023-04-09 17:17:01 +0200

    add deploy scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,14 @@ require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-tracer");
 require("hardhat-gui");
 
+require('dotenv').config();
+
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.POLYGON_MUMBAI_PRIVATE_KEY]
+    }
+  }
 };
